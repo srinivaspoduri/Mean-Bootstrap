5dfd9fc2348e9e6b21952d972a7012a86c0ee035
= <=>
2020-12-06 04:22:21 +0530

    added server start code in package.json

--- server.js
+++ server.js
@@ -13,4 +13,5 @@ res.sendFile(path.join(__dirname+'/dist/MyShopping-BS/index.html'));
 });
 
 // Start the app by listening on the default Heroku port  .....
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
+console.log('server listening on port 3000')
